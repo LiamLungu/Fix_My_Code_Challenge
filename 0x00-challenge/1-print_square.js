5897dea29026fc59712a0ae90962db7d4df06427
Liam Lungu <Dragoneyeyt2020@gmail.com>
2024-05-31 06:40:13 -0700

    Updated

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,22 +6,19 @@
     of the program.
 */
 
-// Check if size argument is provided
+
 if (process.argv.length <= 2) {
-    console.error("You forgot to tell me how big to make the square!");
-    console.error("Usage: ./1-print_square.js <size>");
-    console.error("Example: ./1-print_square.js 8");
-    process.exit(1);
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
 }
 
-// Convert the size argument to a number
-const size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10)
 
-// Print the square
-for (let i = 0; i < size; i++) {
-    let line = "";
-    for (let j = 0; j < size; j++) {
-        line += "#"; // Add a '#' to the line
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
     }
-    console.log(line); // Print the line
+    process.stdout.write("\n");
 }
