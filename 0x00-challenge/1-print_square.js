25abce36eefa9af702dd68c689bbd250414df05d
Liam Lungu <Dragoneyeyt2020@gmail.com>
2024-05-31 06:26:44 -0700

    Fix My Code Challenge

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,17 +1,24 @@
 #!/usr/bin/node
+/*
+    Print a square with the character #
+
+    The size of the square must be the first argument 
+    of the program.
+*/
+
 
 if (process.argv.length <= 2) {
-    console.log("Missing argument");
-    console.log("Usage: ./1-print_square.js <size>");
-    process.exit(1);
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
 }
 
-let size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10)
 
-for (let i = 0; i < size; i++) {
-    let row = "";
-    for (let j = 0; j < size; j++) {
-        row += "#";
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
     }
-    console.log(row);
+    process.stdout.write("\n");
 }
